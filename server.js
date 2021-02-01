53daba2b85f997f957b42079457ddaaada0df5be
Gabriel Koch <gabriel@bespinian.io>
2021-02-01 15:07:34 +0100

    Add prometheus metrics

--- server.js
+++ server.js
@@ -1,16 +1,22 @@
-var express = require('express');
-var app = express();
+const express = require("express");
+const promBundle = require("express-prom-bundle");
+
+const app = express();
+const metricsMiddleware = promBundle({ includeMethod: true });
+
+app.use(metricsMiddleware);
+app.use(express.json());
 
 let port = process.env.PORT || 8080;
 
-app.get('/', (req, res) => {
-    res.send("Hello, World!")
-})
-app.get('/random-nr', function(req, res) {
-    let randomNr = 4 // Random number chosen by fair dice roll
-    res.send(`${randomNr}`);
-})
+app.get("/", (req, res) => {
+  res.json({ hello: "World!" });
+});
+app.get("/random-nr", function (req, res) {
+  let randomNr = 4; // Random number chosen by fair dice roll
+  res.json({ randomValue: `${randomNr}` });
+});
 
-var server = app.listen(port, function() {
-    console.log(`Example app listening on port ${port}`)
-})
+var server = app.listen(port, function () {
+  console.log(`Example app listening on port ${port}`);
+});

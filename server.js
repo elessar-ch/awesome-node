99df03418fd4b19824443be666179114bd2a6b23
Gabriel Koch <gabriel@bespinian.io>
2021-02-02 16:14:51 +0100

    Implement allocate-memory-kb endpoint

--- server.js
+++ server.js
@@ -8,9 +8,13 @@ const metricsMiddleware = promBundle({ includeMethod: true });
 app.use(metricsMiddleware);
 app.use(express.json());
 
+const leak_increment_kb = parseInt(
+  process.env.HEALTHCHECK_LEAK_INCREMENT_KB || 1000
+);
 let port = process.env.PORT || 8080;
+let memory_waste = [];
+let total_wasted_kb = 0;
 
-// let memoryDump = [];
 app.get("/", (req, res) => {
   res.json({ hello: "World!" });
 });
@@ -18,23 +22,41 @@ app.get("/random-nr", function (req, res) {
   let randomNr = 4; // Random number chosen by fair dice roll
   res.json({ randomValue: `${randomNr}` });
 });
+app.post("/allocate-memory-kb", (req, res) => {
+  if (!req.body.memory_size_kb) {
+    res
+      .status(400)
+      .send("Bad Request: must send json body with memory_size_kb property");
+    return;
+  }
+  let increment_kb = parseInt(req.body.memory_size_kb);
+  let increment_byte = increment_kb * 1000;
+  let increment = "*".repeat(increment_byte);
+
+  memory_waste.push(increment);
+  total_wasted_kb += increment_kb;
+  res.json({
+    alive: true,
+    total_wasted_kb: total_wasted_kb,
+    // Make sure the data is kept in the working set by accessing it
+    lastByte: memory_waste[memory_waste.length - 1].charCodeAt(
+      increment_byte - 1
+    ),
+  });
+});
 app.get("/healthcheck", (req, res) => {
   if (process.env.HEALTHCHECK_LEAK_MEMORY) {
-    if (!this.memorydump) {
-      this.memorydump = [];
-      this.i = 0;
-    }
+    let increment_byte = leak_increment_kb * 1000;
+    let increment = "*".repeat(increment_byte);
 
-    this.increment_size = process.env.HEALTHCHECK_LEAK_INCREMENT_KB || 1000;
-    let increment = "*".repeat(this.increment_size * 1000);
-
-    this.memorydump.push(increment);
+    memory_waste.push(increment);
+    total_wasted_kb += leak_increment_kb;
     res.json({
       alive: true,
-      bytes: this.memorydump.length * this.increment_size,
+      total_wasted_kb: total_wasted_kb,
       // Make sure the data is kept in the working set by accessing it
-      lastByte: this.memorydump[this.memorydump.length - 1].charCodeAt(
-        this.increment_size - 1
+      lastByte: memory_waste[memory_waste.length - 1].charCodeAt(
+        increment_byte - 1
       ),
     });
   } else {
